f9eed9923918dad8770edba456e5800a333aa5c0
Camillo <cangelozzi@gmail.com>
2018-10-24 20:01:33 -0400

    Concepts page title and icon

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
   console.log("Welcome to my Blockchain Infographic");
 
   //! VARIABLES
+
+  // repeat animation button with event on click
   const repeat = document.querySelector('#repeat').addEventListener('click', () => {window.location.reload(true);});
 
   // bodymovin script
