095151075ad031d51916fff8451be729bb79ce57
Camillo <cangelozzi@gmail.com>
2018-10-25 20:08:19 -0400

    Concepts page details and media queries

---
+++ js/concepts_data.js
@@ -0,0 +1,44 @@
+const conceptData = [
+  {
+    concept_img: "transaction.svg",
+    concept_title: "transaction",
+    concept_details: "Data sent between an address to another. Bitcoin is famous to create digital financial transaction based on a blockchain structure."
+  },
+  {
+    concept_img: "ledger.svg",
+    concept_title: "ledger",
+    concept_details: "Transactions are recorded and cannot be altered or manipulated. Ledger is shared among all the nodes in the chain."
+  },
+  {
+    concept_img: "node.svg",
+    concept_title: "node",
+    concept_details: "Any computer connected to a has access to ledger and transactions happening in that blockchain."
+  },
+  {
+    concept_img: "distribution.svg",
+    concept_title: "distribution",
+    concept_details: "Transactions are handled without a central authoirty. Blockchain distribution is decentralized in a p2p structure."
+  },
+  {
+    concept_img: "block.svg",
+    concept_title: "block",
+    concept_details: "A Block is a container for a group of transactions added together. Before joining the chain, the block must be validated and hashed."
+  },
+  {
+    concept_img: "blockchain.svg",
+    concept_title: "blockchain",
+    concept_details: "A mathematical structure for storing transactions (data) in an immutable p2p ledger, incredibly difficult to break."
+  },
+  {
+    concept_img: "consensus.svg",
+    concept_title: "consensus",
+    concept_details: "When a majority of partecipants of a networks - nodes - agree on the validity of a transaction."
+  },
+  {
+    concept_img: "bitcoin.svg",
+    concept_title: "reward",
+    concept_details: "A computer solving the right hashing calculation - miner - is rewarded in coins when the block is accepted in the blockchain."
+  },
+
+
+];
