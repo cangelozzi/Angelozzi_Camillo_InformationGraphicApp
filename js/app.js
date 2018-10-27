d5772872f1cf44d5708fa67be1055dbdb5d288ab
Camillo <cangelozzi@gmail.com>
2018-10-26 20:20:26 -0400

    concepts populating from object

--- js/app.js
+++ js/app.js
@@ -17,5 +17,29 @@
 
   //! FUNCTIONS
 
+  // popoulate concepts details
+  function concepts(data) {
+    
+    // title
+    let titles = document.querySelectorAll('.title');
+    titles.forEach((title, index) => {
+      title.textContent = data[index].concept_title;
+    });
+
+    // description
+    let descriptions = document.querySelectorAll('.desc');
+    descriptions.forEach((desc, index) => {
+      desc.textContent = data[index].concept_details;
+    });
+
+    // images
+    let concepts_imgs = document.querySelectorAll('.concepts_grid-icon');
+    concepts_imgs.forEach((img, index) => {
+      img.src += data[index].concept_img;
+    });
+
+  }
+
   //! EVENTS
+  concepts(conceptData);
 })();
